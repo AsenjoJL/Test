d0d69890b8e7c927009ac7e3b0dfe999c06317e6
AsenjoJL <asenjojohnlester24@gmail.com>
2025-03-27 20:26:34 +0800

    Update app.js

--- app.js
+++ app.js
@@ -24,3 +24,23 @@ router.post('/', (req, res) => {
 });
 
 module.exports = router;
+
+
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Import the exams route
+const examsRoute = require('./routes/exams');
+
+// Use the exams route
+app.use('/exams', examsRoute);
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
